7f3ffa8b25de00911a49dd53983f1291c489c92f
Phil Reynolds <gpg@purpleriver.dev>
2025-02-27 07:43:46 +0000

    Check sign up flow with connection link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   add_flash_types :error, :info, :warning, :success
   allow_browser versions: :modern
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name, :town ])
+  end
 end
